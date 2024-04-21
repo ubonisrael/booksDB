5ea68c3e394d963c8722d03aa0dc4134e3edc720
ubonisrael <akpanudohubonisrael@gmail.com>
2024-04-21 14:36:38 +0100

    fixed bug in auth error - added default status code

--- errors/auth_error.ts
+++ errors/auth_error.ts
@@ -1,6 +1,8 @@
+import { StatusCodes } from "http-status-codes";
 import CustomAPIError from "./custom_api";
 
 class UnauthenticatedError extends CustomAPIError {
+    statusCode = StatusCodes.FORBIDDEN;
     constructor(message: string) {
         super(message)
     }
