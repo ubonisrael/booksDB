88b4b35dd677147833db9b9950736960f36b6889
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-05 23:18:57 +0100

    created an util function for updating a book's rating whenever a review is made or updated

--- db/client.ts
+++ db/client.ts
@@ -1,3 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+export const updateBookRating = async (bookId: string) => {
+  const book = await prisma.book.findUnique({
+    where: {
+      id: bookId,
+    },
+    include: {
+      reviews: true,
+    },
+  });
+  
+  if (book) {
+    const newRating =
+      book.reviews.reduce((acc, b) => acc + b.vote, 0) / book.reviews.length;
+    await prisma.book.update({
+      where: {
+        id: book.id,
+      },
+      data: {
+        rating: newRating,
+      },
+    });
+  }
+};
