f98c384b64b6095423e14a221f804b63d2f01096
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-06 09:00:54 +0100

    added support for pagination and filtering for the user model fixed bug when deleting user by making sure related records are deleted first added functionality for adding and removing books from a user's favorites

--- controllers/users/users.ts
+++ controllers/users/users.ts
@@ -2,7 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { prisma } from "../../db/client";
 import { compare, genSalt, hash } from "bcryptjs";
 import { StatusCodes } from "http-status-codes";
-import { forbiddenAttr, userSchema } from "../../schema/validationSchema";
+import {
+  forbiddenAttr,
+  updateFavoriteSchema,
+  userSchema,
+} from "../../schema/validationSchema";
 import jwt from "jsonwebtoken";
 import BadRequestError from "../../errors/bad_request";
 import UnauthenticatedError from "../../errors/auth_error";
@@ -14,10 +18,27 @@ export const getAllUsers = async (
   next: NextFunction
 ) => {
   try {
-    const users = (await prisma.user.findMany()).sort();
-    res.status(StatusCodes.OK).json({ users });
+    // added support for pagination, filtering (limit, offset)
+    const pageNumber = Number(req.query["page"]) || 1;
+    const limit = Number(req.query["limit"]) || 10;
+    const skip = (pageNumber - 1) * limit;
+    const searchRegex = new RegExp(`${req.query["search"] || ""}`, "gi");
+    const users = (
+      await prisma.user.findMany({
+        skip: skip,
+        take: limit,
+      })
+    )
+      .sort()
+      .filter(
+        (us) =>
+          searchRegex.test(us.firstName) ||
+          searchRegex.test(us.lastName) ||
+          searchRegex.test(us.username)
+      );
+    res.status(StatusCodes.OK).json({ users, count: users.length });
   } catch (e) {
-    next(e)
+    next(e);
   }
 };
 
@@ -49,27 +70,7 @@ export const Login = async (
       process.env.JWT_SECRET as string,
       { expiresIn: process.env.JWT_EXPIRES_IN }
     );
-    return res.status(StatusCodes.OK).json({ user, token });
-  } catch (e) {
-    next(e);
-  }
-};
-
-export const getUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!user) {
-      throw new NotFoundError(`user with id-${req.params.id} does not exist.`);
-    }
-    res.status(StatusCodes.OK).json({ user });
+    return res.status(StatusCodes.OK).json({ userId: user["id"], token });
   } catch (e) {
     next(e);
   }
@@ -117,21 +118,20 @@ export const createUser = async (
     const user = await prisma.user.create({
       data: value,
     });
+
     res.status(StatusCodes.CREATED).json({ user });
   } catch (e) {
     next(e);
   }
 };
 
-
-// *** CHANGE TO UPDATE PASSWORD LATER
+// *** CHANGE TO UPDATE PASSWORD LATER ?? Or add password update functionality
 export const updateUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-
     for (const x of forbiddenAttr) {
       if (x in req.body) delete req.body[x];
     }
@@ -162,16 +162,110 @@ export const deleteUser = async (
   next: NextFunction
 ) => {
   try {
-
-    const user = await prisma.user.delete({
+    const deleteReviews = prisma.review.deleteMany({
+      where: {
+        authorId: req.params.id,
+      },
+    })
+    const deleteUser = prisma.user.delete({
       where: {
         id: req.params.id,
       },
     });
+    await prisma.$transaction([deleteReviews, deleteUser])
+    res.status(StatusCodes.OK).json({ status: "success" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const getUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        favoriteBooks: true,
+        reviews: true,
+      },
+    });
     if (!user) {
-      throw new NotFoundError(`User with id-${req.params.id} does not exist.`);
+      throw new NotFoundError(`user with id-${req.params.id} does not exist.`);
     }
-    res.status(StatusCodes.OK).json({});
+    res.status(StatusCodes.OK).json({ user });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const addBookToFavorites = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { bookId } = req.body;
+
+    const { error, value } = updateFavoriteSchema.validate({ bookId });
+
+    if (error) throw new BadRequestError(error.message);
+
+    const user = await prisma.user.update({
+      where: {
+        id: req.user?.userId,
+      },
+      data: {
+        favoriteBooks: {
+          set: {
+            id: value.bookId,
+          },
+        },
+      },
+    });
+
+    if (!user)
+      throw new NotFoundError(`User with id-${req.user?.userId} not found.`);
+
+    res.status(StatusCodes.OK).json({ status: "success" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const removeBookFromFavorites = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { bookId } = req.body;
+
+    const { error, value } = updateFavoriteSchema.validate({ bookId });
+
+    if (error) throw new BadRequestError(error.message);
+
+    const user = await prisma.user.update({
+      where: {
+        id: req.user?.userId,
+      },
+      data: {
+        favoriteBooks: {
+          delete: {
+            id: value.bookId,
+          },
+        },
+      },
+    });
+
+    if (!user)
+      throw new NotFoundError(`User with id-${req.user?.userId} not found.`);
+
+    res.status(StatusCodes.OK).json({ status: "success" });
   } catch (e) {
     next(e);
   }
