b126687b61baf1e6aeb296c709a5f6cf994bf66a
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-14 12:23:16 +0100

    added favorites route to the user route handler

--- routes/users/user.ts
+++ routes/users/user.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { createUser as createAdmin, deleteUser, getAllUsers, getUser, updateUser } from "../../controllers/users/users";
+import {
+  addBookToFavorites,
+  createUser as createAdmin,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  removeBookFromFavorites,
+  updateUser,
+} from "../../controllers/users/users";
 
-export const route = Router()
+export const route = Router();
 
-route.route('/').get(getAllUsers).post(createAdmin)
-route.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
+route.route("/").get(getAllUsers).post(createAdmin);
+route.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+route
+  .route("/:id/favorites")
+  .patch(addBookToFavorites)
+  .delete(removeBookFromFavorites);
