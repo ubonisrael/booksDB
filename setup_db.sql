2bf7d0cdae9e9ff5fc149a2e91bb130d43ff1d1a
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-19 11:12:02 +0100

    changed implicit many-to-many relations to explicit version in prisma.schema

--- setup_db.sql
+++
@@ -1,8 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS whatif_db;
-CREATE USER IF NOT EXISTS 'whatif_admin'@'localhost' IDENTIFIED BY 'Password_123';
-GRANT CREATE, ALTER, DROP, REFERENCES ON *.* TO 'whatif_admin'@'localhost';
-GRANT ALL PRIVILEGES ON `whatif_db`.* TO 'whatif_admin'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'whatif_admin'@'localhost';
-FLUSH PRIVILEGES;
